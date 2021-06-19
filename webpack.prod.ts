fac33354aad141b94622819f9a9c859eac29ddb6
Ari Markowitz <ariymarkowitz@gmail.com>
2021-06-19 15:25:15 +1200

    Create separate dev and production configurations

---
+++ webpack.prod.ts
@@ -0,0 +1,80 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+import path from 'path';
+import { theme } from './src/style/themes/themes';
+const sass = require('sass');
+const sassUtils = require('node-sass-utils')(sass);
+
+module.exports = {
+  mode: 'production',
+  entry: [
+    './src/main.tsx',
+    './src/style/main.scss'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.exec\.js$/,
+        use: [ 'script-loader' ]
+      }, {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: '.',
+              name: '[name].css',
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                functions: {
+                  "get($keys)": function(_keys: any) {
+                    const keys = _keys.getValue().split(".");
+                    let result: any = theme;
+                    let i;
+                    for (i = 0; i < keys.length; i++) {
+                      result = result[keys[i]];
+                    }
+                    result = sassUtils.castToSass(result);
+                    return result;
+                  }
+                },
+              }
+            }
+          }
+        ]
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    minimize: true
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Bruhat-Tits Tree',
+    template: 'src/index.html',
+    files: {
+      js: [ "dist/bundle.js"],
+    }
+  }),],
+};
