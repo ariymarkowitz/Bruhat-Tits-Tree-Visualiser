05ca4007f14b728c2edfefd36f1aed2991bffb87
Ari Markowitz <ariymarkowitz@gmail.com>
2022-04-19 01:34:12 +1200

    Fix interpolation of angles

--- src/Tree/TreeRenderer.ts
+++ src/Tree/TreeRenderer.ts
@@ -15,6 +15,9 @@ export interface TreeOptions {
   showEnd: boolean
 }
 
+/**
+ * The state known to a vertex, regardless of choice of origin.
+ */
 interface LocalState {
   depth: number
   isLeaf: boolean
@@ -23,20 +26,31 @@ interface LocalState {
   isMinTranslation: boolean
 }
 
+/**
+ * The state known to a (directed) edge, regardless of choice of origin.
+ */
 interface EdgeState {
   depth: number
   forward: number
   backward: number
 }
 
+/**
+ * The global state calculated for each vertex, accumulated via the local state
+ */
 interface GlobalState {
   x: number
   y: number
+  angle: number
+  // Only used to set the angle of the image of the origin.
   zeroAngle: number
   depth: number
   edgeDepth: number
 }
 
+/**
+ * The state used to draw a vertex.
+ */
 interface VertexGraphicsState {
   x: number
   y: number
@@ -45,6 +59,9 @@ interface VertexGraphicsState {
   strokeColor: string
 }
 
+/**
+ * The state used to draw an edge.
+ */
 interface EdgeGraphicsState {
   x1: number
   y1: number
@@ -59,6 +76,12 @@ interface IsoInfo {
   minDist: number
 }
 
+/**
+ * Stores all the data needed to render the tree.
+ * 
+ * All values that need interpolating are stored in maps,
+ * to speed up calculation.
+ */
 export class TreeRenderer {
   width: number
   height: number
@@ -74,9 +97,28 @@ export class TreeRenderer {
 
   btt: BruhatTitsTree
   
+  /**
+   * The local states of each vertex.
+   */
   states: Map<string, LocalState>
+  /**
+   * The images of each vertex.
+   */
   images: Map<string, Vertex>
+  /**
+   * The state of (directed) edges of neighbouring vertices.
+   */
   edges: Map<string, EdgeState>
+  /**
+   * The angles of each vertex, relative to the parent.
+   * This is stored in the vertex instead of the edge to avoid
+   * the need for parent edges.
+   */
+  angles: Map<string, number>
+  /**
+   * The angle of the image of each vertex, relative to the parent.
+   */
+  imageAngles: Map<string, number>
 
   loopTime = 2000
 
@@ -119,6 +161,8 @@ export class TreeRenderer {
     this.states = new Map()
     this.images = new Map()
     this.edges = new Map()
+    this.angles = new Map()
+    this.imageAngles = new Map()
 
     this.initVertex = this.btt.origin
 
@@ -144,23 +188,20 @@ export class TreeRenderer {
       return {value: newState, stop: newState.isLeaf}
     }, initLocalState, this.initVertex)
 
-    // Set the states of the images of the rendered vertices.
-    // We do this after rendering all vertices so that we don't have to recalculate vertices that are already determined.
-    this.btt.iterVertices(vertex => {
-      const state = this.getLocalState(vertex)
-      const image = this.getImage(vertex)
-
-      this.setVertexMapOnce(this.states, image, this.calculateLocalState(image, depth + 1, true))
-
-      return state.isLeaf
-    }, this.initVertex)
-
-    // Set the state of the images of the edges.
+    // Set the state of the images of the vertices and edges.
     this.btt.iter((_, current, update) => {
       const state = this.getLocalState(update.vertex)
 
       const image1 = this.getImage(current)
       const image2 = this.getImage(update.vertex)
+
+      const key = this.btt.vertexToString(image2)
+      if (!this.states.has(key)) {
+        // TODO: This assumes that the image of the origin is still in the original tree.
+        // Is this worth fixing?
+        const newImageState = this.calculateLocalState(image2, this.getLocalState(image1).depth + 1, true)
+        this.states.set(key, newImageState)
+      }
 
       const state1 = this.getLocalState(image1)
       const state2 = this.getLocalState(image2)
@@ -175,20 +216,39 @@ export class TreeRenderer {
       return {value: undefined, stop: state.isLeaf}
     }, undefined, this.initVertex)
 
+    // Set the angle of the vertices.
+    const HALFANGLE = (this.p+1)/2
+    this.setAngle(this.initVertex, HALFANGLE)
+    this.setImageAngle(this.initVertex, HALFANGLE)
+    this.btt.iter((state, parent, update) => {
+      const edge = this.getEdgeState(parent, update.vertex)
+      const imageEdge = this.getEdgeState(this.getImage(parent), this.getImage(update.vertex))
+      const angle = edge.forward
+      const imageAngle = imageEdge.forward
+      this.setAngle(update.vertex, angle - state.angle - HALFANGLE)
+      this.setImageAngle(update.vertex, imageAngle - state.imageAngle - HALFANGLE)
+
+      return {value: {angle: edge.backward, imageAngle: imageEdge.backward}, stop: this.getLocalState(update.vertex).isLeaf}
+    }, {angle: this.getAngle(this.initVertex), imageAngle: this.getImageAngle(this.initVertex)}, this.initVertex)
+
+    // Set the global state of the origin.
     this.originGlobalState = {
       x: width/2,
       y: height/2,
       zeroAngle: 0,
+      angle: 0, //2*Math.PI*(1/(this.p+1) - 1/4),
       depth: 0,
       edgeDepth: 0
     }
 
-    // Set the global state of the image.
+    // Set the global state of the image of the origin.
     this.imageGlobalState = this.btt.reducePath((globalState: GlobalState, v: Vertex, adj: Adj<Vertex, number>) => {
       const localState = this.getLocalState(adj.vertex)
       const edgeState = this.getEdgeState(v, adj.vertex)
-      return this.accumulate(globalState, localState, edgeState)
+      const angle = this.getAngle(adj.vertex)
+      return this.accumulate(globalState, localState, edgeState, angle)
     }, this.initVertex, this.getImage(this.initVertex), this.originGlobalState)
+    this.imageGlobalState.angle = this.imageGlobalState.zeroAngle
   }
 
   calculateLocalState(v: Vertex, depth: number, isLeaf: boolean): LocalState {
@@ -261,14 +321,31 @@ export class TreeRenderer {
     return value
   }
 
-  accumulate(state: GlobalState, local: LocalState, edge: EdgeState): GlobalState {
-    const scale = Math.pow(0.8 * Math.pow(1/this.p, 0.45), edge.depth - 1) * Math.pow(this.p, 0.2)
-    const angle = state.zeroAngle - 2*Math.PI/(this.p+1)*edge.forward
+  setAngle(v: Vertex, angle: number) {
+    this.setVertexMapOnce(this.angles, v, angle)
+  }
 
+  getAngle(v: Vertex): number {
+    return this.getVertexMap(this.angles, v)
+  }
+
+  setImageAngle(v: Vertex, angle: number) {
+    this.setVertexMapOnce(this.imageAngles, v, angle)
+  }
+
+  getImageAngle(v: Vertex): number {
+    return this.getVertexMap(this.imageAngles, v)
+  }
+
+  accumulate(state: GlobalState, local: LocalState, edge: EdgeState, prevAngle: number): GlobalState {
+    const scale = Math.pow(0.8 * Math.pow(1/this.p, 0.45), edge.depth - 1) * Math.pow(this.p, 0.2)
+    const angle = state.angle - 2*Math.PI*prevAngle/(this.p+1)
+    
     return {
       x: state.x + 160 * scale * Math.cos(angle),
       y: state.y + 160 * scale * Math.sin(angle),
-      zeroAngle: angle + 2*Math.PI/(this.p+1)*edge.backward + Math.PI,
+      zeroAngle: angle + Math.PI + edge.backward * 2*Math.PI/(this.p + 1),
+      angle,
       depth: local.depth,
       edgeDepth: edge.depth
     }
@@ -297,9 +374,14 @@ export class TreeRenderer {
       x: lerp(state1.x, state2.x, t),
       y: lerp(state1.y, state2.y, t),
       zeroAngle: angleLerp(state1.zeroAngle, state2.zeroAngle, t),
+      angle: angleLerp(state1.angle, state2.angle, t),
       depth: lerp(state1.depth, state2.depth, t),
       edgeDepth: lerp(state1.edgeDepth, state2.edgeDepth, t)
     }
+  }
+
+  interpAngle(angle1: number, angle2: number, t: number): number {
+    return radialLerp(angle1, angle2, t, this.p+1)
   }
 
   vertexColor(state: LocalState): string {
@@ -399,11 +481,15 @@ export class TreeRenderer {
       const edgeState = this.getEdgeState(current, update.vertex)
       const imageEdgeState = this.getEdgeState(prevImage, image)
 
+      const angle = this.getAngle(update.vertex)
+      const imageAngle = this.getImageAngle(update.vertex)
+
       const newLocalState = this.interpLocalStates(localState, imageLocalState, i)
       const newGlobalState = this.accumulate(
         prevGlobalState,
         newLocalState,
-        this.interpEdgeStates(edgeState, imageEdgeState, i)
+        this.interpEdgeStates(edgeState, imageEdgeState, i),
+        this.interpAngle(angle, imageAngle, i)
       )
       const vertexGraphicsState = this.makeVertexGraphicsState(newLocalState, newGlobalState)
       const edgeGraphicsState = this.makeEdgeGraphicsState(prevLocalState, prevGlobalState, newLocalState, newGlobalState)
