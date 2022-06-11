2df1fc9169b94c0fa1fdc74f0aab692b7becefb2
Ari Markowitz <ariymarkowitz@gmail.com>
2022-06-11 19:20:47 +1200

    Update color scheme

--- src/style/themes/mocha.ts
+++ src/style/themes/mocha.ts
@@ -1,7 +1,7 @@
 export default {
   name: 'Mocha',
   ui: {
-    background: '#f2cf9d',
+    background: '#d9bea6',
     textColor: '#60503B',
     border: '#60503B',
     focusBorder: '#261702',
@@ -9,13 +9,13 @@ export default {
   },
   tree: {
     type0: '#60503B',
-    type1: '#f2cf9d',
+    type1: '#d9bea6',
     vertexStroke: '#60503B',
     edge: '#60503B',
     end: '#ae6642',
     fixedPoints: '#015d6f',
     translationAxis: '#bf3933',
-    highlightVertex: '#d2ac88',
+    highlightVertex: '#fce4c0',
     branchWidth: 5,
     vertexStrokeWidth: 2.5,
     vertexRadius: 10,
